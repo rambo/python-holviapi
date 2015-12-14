f0f74132b12c45d19266b7d42449b4bb4a250cbd
Eero af Heurlin <rambo@iki.fi>
2015-12-14 20:09:44 +0200

    Use sessioned http-requests, allows connection pooling etc

--- holviapi/connection.py
+++ holviapi/connection.py
@@ -7,17 +7,20 @@ import requests
 @python_2_unicode_compatible
 class Connection(object):
     base_url_fmt = "https://holvi.com/api/"
+    session = None
     
     def __init__(self, poolname, authkey):
         self.pool = poolname
         self.key = authkey
 
     def make_get(self, url, params={}):
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Token %s' % self.key
-        }
-        r = requests.get(url, headers=headers, params=params)
+        if not self.session:
+            self.session = requests.Session()
+            self.session.headers.update({
+                'Content-Type': 'application/json',
+                'Authorization': 'Token %s' % self.key
+            })
+        r = self.session.get(url, params=params)
         r.raise_for_status()
         return r.json()
 
