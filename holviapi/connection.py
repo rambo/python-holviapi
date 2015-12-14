be07df17d1834cf87ab97c4a7890b138623e1b2e
Eero af Heurlin <rambo@iki.fi>
2015-12-14 21:44:54 +0200

    Add POST/PUT/PATCH methods to connection
    Also make the error classes better.

--- holviapi/connection.py
+++ holviapi/connection.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-from future.utils import python_2_unicode_compatible
+from future.utils import python_2_unicode_compatible, raise_from
+import six
+import json
 import requests
+from requests.exceptions import HTTPError, Timeout
 import requests_cache
+from .errors import AuthenticationError, ApiError, ApiTimeout
+
 # Cache GET results for 5min to save Holvis bandwidth (also the API is a bit on the slow side so this makes things faster for us)
 requests_cache.install_cache('Holvi-REST', 'memory', expire_after=300)
 
@@ -15,18 +20,54 @@ class Connection(object):
         self.pool = poolname
         self.key = authkey
 
-    def make_get(self, url, params={}):
+    def _init_session(self):
         if not self.session:
             self.session = requests.Session()
             self.session.headers.update({
                 'Content-Type': 'application/json',
                 'Authorization': 'Token %s' % self.key
             })
+
+    def make_get(self, url, params={}):
+        self._init_session()
         r = self.session.get(url, params=params)
-        r.raise_for_status()
+        try:
+            r.raise_for_status()
+        except Timeout as e:
+            raise_from(ApiTimeout(six.u(e), response=e.response), e)
+        except HTTPError as e:
+            if e.response.status_code in (403, 401):
+                raise_from(AuthenticationError(six.u(e), response=e.response), e)
+            else:
+                raise_from(ApiError(six.u(e), response=e.response), e)
         return r.json()
 
-    def make_post(self, url, params, method='POST'):
+    def make_post(self, url, payload):
+        """Make a POST request"""
+        return self._make_ppp('post', url, payload)
+
+    def make_put(self, url, payload):
+        """Make a PUT request"""
+        return self._make_ppp('put', url, payload)
+
+    def make_patch(self, url, payload):
+        """Make a PATCH request"""
+        return self._make_ppp('patch', url, payload)
+
+    def _make_ppp(self, method, url, payload):
+        """Internal helper to make POST/PUT/PATCH requests (or whatever the underlying library supports)"""
+        self._init_session()
         # We can't trust the cache after we have made changes of our own
         requests_cache.clear()
-        raise NotImplementedError()
+        m = getattr(self.session, method)
+        r = m(url, data=json.dumps(payload))
+        try:
+            r.raise_for_status()
+        except Timeout as e:
+            raise_from(ApiTimeout(six.u(e), response=e.response), e)
+        except HTTPError as e:
+            if e.response.status_code in (403, 401):
+                raise_from(AuthenticationError(six.u(e), response=e.response), e)
+            else:
+                raise_from(ApiError(six.u(e), response=e.response), e)
+        return r.json()
--- holviapi/errors/__init__.py
+++ holviapi/errors/__init__.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 from future.utils import python_2_unicode_compatible
+from requests.exceptions import HTTPError, Timeout
 
 @python_2_unicode_compatible
 class HolviError(RuntimeError):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(HolviError, self).__init__(*args, **kwargs)
 
-class ApiError(HolviError):
-    pass
+class ApiError(HTTPError, HolviError):
+    def __init__(self, *args, **kwargs):
+        super(ApiError, self).__init__(*args, **kwargs)
 
-class AuthenticationError(HolviError):
-    pass
+class AuthenticationError(ApiError):
+    def __init__(self, *args, **kwargs):
+        super(AuthenticationError, self).__init__(*args, **kwargs)
 
-class NotFound(HolviError):
-    pass
+class ApiTimeout(ApiError, Timeout):
+    def __init__(self, *args, **kwargs):
+        super(ApiTimeout, self).__init__(*args, **kwargs)
+
