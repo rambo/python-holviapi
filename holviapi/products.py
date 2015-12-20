e9f5d7103030d1088b412b463a9cb30f9d973667
Eero af Heurlin <rambo@iki.fi>
2015-12-20 21:51:46 +0200

    Improve the product class
    Add category mapping to Category object etc

--- holviapi/products.py
+++ holviapi/products.py
@@ -1,12 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 from future.utils import python_2_unicode_compatible, raise_from
+from .categories import IncomeCategory, CategoriesAPI
 from .utils import HolviObject
 
 
 @python_2_unicode_compatible
 class Product(HolviObject):
     """This represents a product in the Holvi system"""
+    api = None
+    category = None
+    _cklass = IncomeCategory
+    _valid_keys = ["code", "name", "description"] # Not really, there is no API for managing products ATM
+
+    def __init__(self, api, jsondata=None, cklass=None, **kwargs):
+        if cklass:
+            self._cklass = cklass
+        self._fetch_method = api.get_product
+        super(Product, self).__init__(api, jsondata, **kwargs)
+
+    def _map_holvi_json_properties(self):
+        if self._jsondata.get("category"):
+            self.category = self._cklass(self.api.categories_api, {"code": self._jsondata["category"]})
+
+    def to_holvi_dict(self, patch=False):
+        if self.category:
+            self._jsondata["category"] = self.category.code
+        filtered = { k:v for (k,v) in self._jsondata.items() if k in self._valid_keys }
+        return filtered
 
 
 class ShopProduct(Product):
@@ -27,6 +48,7 @@ class ProductsAPI(object):
 
     def __init__(self, connection):
         self.connection = connection
+        self.categories_api = CategoriesAPI(self.connection)
         self.base_url = str(connection.base_url_fmt + self.base_url_fmt).format(pool=connection.pool)
 
     def list_products(self):
@@ -39,3 +61,13 @@ class ProductsAPI(object):
         for pjson in obdata['products']:
             ret.append(ShopProduct(self, pjson))
         return ret
+
+    def get_product(self, code):
+        """Gets category with given code
+
+        NOTE: Filters the list of income and expense categories in this end due to
+        API limitations"""
+        candidates = filter(lambda c: c.code == code, self.list_products())
+        if not candidates:
+            return None
+        return next(candidates)
