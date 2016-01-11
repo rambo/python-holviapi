da23ae3cb5f0abb2cda7b40902745fc37ccc572c
Eero af Heurlin <rambo@iki.fi>
2016-01-12 00:21:59 +0200

    Import some new names to the module namespace

--- holviapi/__init__.py
+++ holviapi/__init__.py
@@ -2,6 +2,7 @@
 from .errors import *
 from .connection import Connection
 from .invoicing import Invoice, InvoiceItem, InvoiceAPI
-from .checkout import Order, CheckoutAPI
+from .checkout import Order, CheckoutAPI, CheckoutItemAnswer, CheckoutItem
+from .contacts import OrderContact
 from .products import ShopProduct, OrderProduct, ProductsAPI
 from .categories import IncomeCategory, ExpenseCategory, CategoriesAPI
