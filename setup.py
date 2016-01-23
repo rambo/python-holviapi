0f8e1147b70898d2196bebfcd219f9ed803b73df
Eero af Heurlin <eero.afheurlin@iki.fi>
2016-01-23 19:33:22 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,21 +5,9 @@ except ImportError:
     from distutils.core import setup
 import subprocess
 
-import six
-
-git_version = 'UNKNOWN'
-try:
-    if six.PY2:
-        git_version = str(subprocess.check_output(['git', 'rev-parse', '--verify', '--short', 'HEAD'])).strip()
-    if six.PY3:
-        git_version = subprocess.check_output(['git', 'rev-parse', '--verify', '--short', 'HEAD']).strip().decode('ascii')
-except subprocess.CalledProcessError:
-    pass
-
 setup(
     name='holviapi',
-    version='0.1.1dev-%s' % git_version,
-    # version='0.1.1',
+    version='0.1.20160123',
     author='Eero "rambo" af Heurlin',
     author_email='rambo@iki.fi',
     packages=['holviapi', 'holviapi.errors', ],
