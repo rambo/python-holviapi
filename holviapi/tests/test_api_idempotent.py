92845765d2762d2b5b6a98d6e86e3a60fd3456f4
Eero af Heurlin <eero.afheurlin@iki.fi>
2017-01-29 01:50:12 +0200

    test fetching invoice

---
+++ holviapi/tests/test_api_idempotent.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import os
+import pytest
+import holviapi
+
+@pytest.fixture
+def connection():
+    pool = os.environ.get('HOLVI_POOL', None)
+    key = os.environ.get('HOLVI_KEY', None)
+    if not pool or not key:
+        raise RuntimeError("HOLVI_POOL and HOLVI_KEY must be in ENV for these tests")
+    cnc = holviapi.Connection(pool,key)
+    return cnc
+
+@pytest.fixture
+def invoiceapi():
+    cnc = connection()
+    ia = holviapi.InvoiceAPI(cnc)
+    return ia
+
+def test_list_invoices(invoiceapi):
+    l = invoiceapi.list_invoices()
+    i = next(l)
+    assert type(i) == holviapi.Invoice
