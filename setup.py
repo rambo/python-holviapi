bb10eb1c7c23dc60829433aea0679117289f5ddf
Eero af Heurlin <eero.afheurlin@iki.fi>
2019-01-30 20:02:22 +0200

    version bump for packaging

--- setup.py
+++ setup.py
@@ -18,11 +18,11 @@ except subprocess.CalledProcessError:
 
 setup(
     name='holviapi',
-    #version='0.5.2dev-%s' % git_version,
-    version='0.5.20190130',
+    #version='0.7.1dev-%s' % git_version,
+    version='0.6.20190130',
     author='Eero "rambo" af Heurlin',
     author_email='rambo@iki.fi',
-    packages=['holviapi', 'holviapi.errors', ],
+    packages=['holviapi', 'holviapi.errors', 'holviapi.tests'],
     license='MIT',
     long_description=open('README.md').read(),
     description='Implement Pythonic wrappers for Holvi JSON-REST API',
